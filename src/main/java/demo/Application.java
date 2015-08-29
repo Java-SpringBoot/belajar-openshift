78aa836c41a3bc64f39a9519cbf7ff5aececb5de
mirzaakhena <mirza.akhena@gmail.com>
2015-08-29 14:13:30 +0700

    rubah pathvariable

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,8 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@RequestMapping("/hello")
-	public ResponseEntity<?> hello() {
-		return new ResponseEntity<>("Test Aplikasi", HttpStatus.OK);
+	@RequestMapping("/{pesan}")
+	public ResponseEntity<?> hello(@PathVariable String pesan) {
+		return new ResponseEntity<>(pesan, HttpStatus.OK);
 	}
 }
