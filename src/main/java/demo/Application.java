ba7c49e79d3ae8e24b33ae1bfbac068d15eeebf2
mirzaakhena <mirza.akhena@gmail.com>
2015-08-29 09:24:29 +0700

    bikin jadi sederhana

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,17 +1,22 @@
 package demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@RestController
+@SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        System.out.println();
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public ResponseEntity<?> hello() {
+		return new ResponseEntity<>("Hallo Semuanya", HttpStatus.OK);
+	}
 }
